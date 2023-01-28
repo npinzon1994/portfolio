c5df7fbcce00e8d3fc5d5cceb886f9b5438edbe5
Nikki Pinzon <npinzon1994@gmail.com>
2023-01-28 12:55:40 -0500

    Added send status markers to Contact form.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,25 +1,35 @@
-import React, { useRef } from "react";
-import classes from './Contact.module.css';
+import React, { useRef, useState } from "react";
+import classes from "./Contact.module.css";
 import emailjs from "@emailjs/browser";
 import Title from "./UI/Title";
 
 const Contact = (props) => {
+  const [isSending, setIsSending] = useState(false);
+  const [sendSuccessful, setSendSuccessful] = useState();
+  const [error, setError] = useState();
+
   const form = useRef();
 
   const submitForm = async (event) => {
     event.preventDefault();
 
     try {
+      setIsSending(true);
       const result = await emailjs.sendForm(
         "service_nuurigj",
         "template_gpqj79d",
         form.current,
         "JioaKW-iFHKYcmt6F"
       );
+
       console.log(result.text);
     } catch (error) {
-      console.log(error.text);
+      setIsSending(false);
+      setSendSuccessful(false);
+      setError(error.text);
     }
+    setIsSending(false);
+    setSendSuccessful(true);
     event.target.reset();
   };
 
@@ -29,7 +39,10 @@ const Contact = (props) => {
       <form onSubmit={submitForm} ref={form} className={classes.form}>
         <input type="text" placeholder="Name" name="name" />
         <input type="email" placeholder="Email" name="email" />
-        <textarea placeholder="Message" name="message" rows="10"/>
+        <textarea placeholder="Message" name="message" rows="10" />
+        {isSending && <p>Sending Email...</p>}
+        {sendSuccessful && !isSending && <p>Email sent successfully!</p>}
+        {error && <p>{error}</p>}
         <button type="submit">Let's Chat</button>
       </form>
     </div>
